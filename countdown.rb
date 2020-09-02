874e19dc123bc7ba09ee3fab7d53999ee1fb3cca
Catherine Gloria <catherinegloria@gmail.com>
2020-09-02 00:46:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,14 @@
 #write your code here
 
 def countdown (number)
-  count = number
-  while
-  puts "#{count} SECONDS!"
+  x = number
+  while x > 0
+  puts "#{x} SECONDS!"
   count -= 1
   end
   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+
 end
