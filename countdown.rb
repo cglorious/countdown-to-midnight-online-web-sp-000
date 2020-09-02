671abab9171d28ac74e0f5637f4cdf59d0fe6653
Catherine Gloria <catherinegloria@gmail.com>
2020-09-02 00:48:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,5 @@
 #write your code here
+require "pry"
 
 def countdown (number)
   while number > 0
@@ -6,6 +7,9 @@ def countdown (number)
   count -= 1
   end
   "HAPPY NEW YEAR!"
+
+binding.pry
+
 end
 
 def countdown_with_sleep
